8c4bc01024876b1631cb73d4f4498349afb4c44f
Godbless Nyagawa <godblessnyagawa@gmail.com>
2025-01-03 10:47:02 +0300

    chore: update ESLint configuration
    - Set sourceType to "commonjs" for all JavaScript files
    - Add browser globals to language options
    - Include recommended ESLint plugin configuration

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,11 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
 
-
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ];
